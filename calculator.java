8f9d657378f995c1310b9e8d655fe21a7470538d
PreetanjaliR <preetanjali08@gmail.com>
2019-01-17 11:08:14 +0530

    Adding addition module

--- calculator.java
+++ calculator.java
@@ -7,10 +7,8 @@ public class calculator{
 
 		do{
 			System.out.print("1. Add two numbers \n");
-			System.out.print("2. Subtract two number \n");
-		 System.out.print("3. Multiply two number \n");
 
-			System.out.print("4. Exit \n \n");
+			System.out.print("2. Exit \n \n");
 			System.out.print("Enter your choice: ");
 			choice=scan.next().charAt(0);
 			switch(choice)
@@ -21,28 +19,14 @@ public class calculator{
 					  res=a+b;
 					  System.out.print("Result= " +res);
 					  break;
-				case '2':System.out.print("Enter two numbers:");
-                                          a=scan.nextFloat();
-                                          b=scan.nextFloat();
-                                          res=a-b;
-                                          System.out.print("Result= " +res);
-                                          break;
-				 case '3':System.out.print("Enter two numbers:");
-                                          a=scan.nextFloat();
-                                          b=scan.nextFloat();
-                                          res=a*b;
-                                          System.out.print("Result= " +res);
-                                          break;
-
-
-
-				case '4': System.exit(0);
+				
+				 case '2':System.exit(0);
 					  break;
 				default: System.out.print("Invalid choice");
 					 break;
 			}
 			System.out.print("\n----------------------\n");
 		}
-		while(choice!=4);
+		while(choice!=2);
 	}
 }
